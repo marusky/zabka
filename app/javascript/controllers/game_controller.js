a2301e58261a4a877383313115eb014856660ee2
marusky <adrianmarusakza@gmail.com>
2022-06-09 12:33:47 +0200

    Add programming level type

--- app/javascript/controllers/game_controller.js
+++ app/javascript/controllers/game_controller.js
@@ -1,28 +1,79 @@
 import ApplicationController from './application_controller.js'
 
 export default class extends ApplicationController {
-    static targets = [ "frog" ]
+    static targets = [ "frog", "frogMoves", "jumpBtn" ]
 
     connect() {
         super.connect();
-        const { initialRow, initialBlock, levelLayout } = this.frogTarget.dataset
-
-        this.frog_position = [initialRow, initialBlock]
-        this.levelLayout = JSON.parse(levelLayout)
-        this.stamp_jump()
     }
 
-    move(e) {
-        this.handle_frog_move(e.key)
-        if (this.is_level_passed()) this.handle_level_passed()
+    keyboard_move(e) {
+        this.move(e.key)
+    }
+
+    click_move({ params: { where } }) {
+        this.move(where.toString())
+    }
+
+    programming_move({ params: { where } }) {
+        this.add_frog_move(where)
+    }
+
+    jump() {
+        const moves = Array.from(this.frogMovesTarget.childNodes).map((node) => node.innerText)
+
+        this.jumpBtnTarget.disabled = true
+        this.jumpBtnTarget.innerText = 'jumping'
+
+        moves.forEach((move, index) => {
+            setTimeout(() => this.move(move), 300 * index)
+        })
+
+        setTimeout(() => {
+            this.jumpBtnTarget.innerText = 'jump'
+        }, 300 * moves.length)
+    }
+
+    reset() {
+        this.initialize()
+        this.resetLayout()
+        this.jumpBtnTarget.disabled = false
+    }
+
+    removeMove(e) {
+        e.target.remove()
     }
 
     // private
 
-    handle_frog_move(key) {
+    initialize() {
+        const { initialRow, initialBlock, levelLayout } = this.frogTarget.dataset
+
+        this.frog_position = [initialRow, initialBlock]
+        this.levelLayout = JSON.parse(levelLayout)
+        this.is_programming = true
+        this.stamp_jump()
+    }
+
+    resetLayout() {
+        this.levelLayout.forEach((row, rowNum) => {
+            row.forEach((oldBlock, blockNum) => {
+                const blockDiv = document.getElementById(`block${rowNum}${blockNum}`)
+                console.log(`${blockDiv.className} -> ${oldBlock}`)
+                blockDiv.className = oldBlock
+            })
+        })
+    }
+
+    move(where) {
+        this.handle_frog_move(where)
+        if (this.is_level_passed()) this.handle_level_passed()
+    }
+
+    handle_frog_move(where) {
         const old_position = [...this.frog_position]
 
-        switch(key) {
+        switch(where) {
             case '9':
                 if (parseInt(this.frog_position[0]) > 0) {
                     this.frog_position[0] = (parseInt(this.frog_position[0]) - 1).toString()
@@ -88,16 +139,23 @@ export default class extends ApplicationController {
 
         }
 
-        this.stamp_jump()
+        this.stamp_jump(where)
+
     }
 
-    stamp_jump() {
+    stamp_jump(where) {
         if (this.levelLayout[this.frog_position[0]][this.frog_position[1]] !== 'j') {
             this.levelLayout[this.frog_position[0]][this.frog_position[1]] = 'j'
             const rockNode = document.getElementById(`block${this.frog_position[0]}${this.frog_position[1]}`)
             rockNode.className = 'j'
             rockNode.appendChild(this.frogTarget)
+
+            if (!this.is_programming) this.add_frog_move(where)
         }
+    }
+
+    add_frog_move(where) {
+        if ('12346789'.includes(where)) this.frogMovesTarget.innerHTML += `<div class="move" data-action="click->game#removeMove">${where}</div>`;
     }
 
     is_level_passed() {
